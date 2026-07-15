6663db9860ae6426e0643dd526f77558a2edb5a5
vitorbase <vitor@igrejabase.org.br>
2026-07-15 13:40:26 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'objetivo-adventure-mobile-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './game.js?v=20260714-mobile-pwa',
+  './manifest.json',
+  './mascote.png',
+  './logo_mascote.jpg',
+  './spritesheet.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
